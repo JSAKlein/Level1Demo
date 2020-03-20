6660cbd90199323fe56831a088e24ef34828d80b
Jeremia Klein <jeremia.klein@hva.nl>
2020-03-20 14:07:31 +0100

    Initial commit

---
+++ app/src/main/java/com/superextra/level1demo/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.superextra.level1demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        confirm.setOnClickListener {
+            checkAnswer()
+        }
+
+    }
+
+    private fun checkAnswer(){
+        val answer = answer.text.toString()
+
+        if (answer == getString(R.string.giraffe)) {
+            Toast.makeText(this, getString(R.string.correct),
+                Toast.LENGTH_LONG).show()
+                    }
+        else {
+            Toast.makeText(
+                this, getString(R.string.incorrect),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+}
